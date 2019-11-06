f13c05eed1d4688149d084bbbb5d675732767d98
YagoLopes <yagolopeslazaro@gmail.com>
2019-11-06 10:08:09 -0300

    add prettierrc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { MatToolbarModule, MatListModule } from "@angular/material";
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatListModule } from '@angular/material';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,4 +16,4 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
