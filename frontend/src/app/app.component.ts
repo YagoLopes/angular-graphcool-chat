1e7a76b13018274b53068ad746dd40edc98451ee
Yago Lopes <yagolopeslazaro@gmail.com>
2019-11-08 16:59:03 -0300

    query and mutation apollo-cliente

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from "graphql-tag";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-graphcool-chat';
+  constructor(
+    private apollo: Apollo,
+  ) {
+    this.createUser();
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    this.apollo.query({
+      query: gql`
+      query{
+        allUsers {
+                  id
+                  name
+                  email
+                }
+              }
+      `
+    }).subscribe(res => console.log("Query:", res));
+
+  }
+
+  createUser(): void {
+    this.apollo.mutate({
+      mutation: gql`
+      mutation createNewUser($name: String!, $email: String!, $password: String!){
+        createUser(name: $name, email:$email, password: $password){
+          id
+          name
+          email
+        }
+       }
+      `,
+      variables: {
+        name: 'Yago Lopes',
+        email: "yagolopeslazaro@gmail.com",
+        password: "123456"
+      }
+    }).subscribe(res => console.log("Mutation:", res))
+  }
+
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApolloConfigModule } from './apollo-config.nodule';
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
+    ApolloConfigModule,
     MatToolbarModule,
     MatListModule
   ],
