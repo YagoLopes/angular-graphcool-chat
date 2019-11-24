5c9da841ed7a4e7dd6d7b7ee283f8c596aebcdd5
Yago Lopes <yagolopeslazaro@gmail.com>
2019-11-24 14:38:06 -0300

    add prettier config tslint

--- frontend/src/app/apollo-config.nodule.ts
+++ frontend/src/app/apollo-config.nodule.ts
@@ -1,26 +1,18 @@
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from '@angular/common/http';
-import { Apollo, ApolloModule } from "apollo-angular";
-import { HttpLink, HttpLinkModule } from "apollo-angular-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { Apollo, ApolloModule } from 'apollo-angular'
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 @NgModule({
-  imports: [
-    HttpClientModule,
-    ApolloModule,
-    HttpLinkModule
-  ]
+    imports: [HttpClientModule, ApolloModule, HttpLinkModule],
 })
 export class ApolloConfigModule {
-  constructor(
-    private apollo: Apollo,
-    private httpLink: HttpLink
-
-  ) {
-    const uri = "https://api.graph.cool/simple/v1/ck2ne3j5l56f30189breb7k5o";
-    const http = httpLink.create({ uri });
-    apollo.create({
-      link: http,
-      cache: new InMemoryCache()
-    })
-  }
+    constructor(private apollo: Apollo, private httpLink: HttpLink) {
+        const uri = 'https://api.graph.cool/simple/v1/ck2ne3j5l56f30189breb7k5o'
+        const http = httpLink.create({ uri })
+        apollo.create({
+            link: http,
+            cache: new InMemoryCache(),
+        })
+    }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ApolloConfigModule } from './apollo-config.nodule';
-
+import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatListModule } from '@angular/material/list'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ApolloConfigModule } from './apollo-config.nodule'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ApolloConfigModule,
-    MatToolbarModule,
-    MatListModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ApolloConfigModule,
+        MatToolbarModule,
+        MatListModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
